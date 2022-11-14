ac9a410f757b59a17377375bc0bdbc24a5fcd250
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-11-14 14:09:04 +0600

    some changes done to db

--- src/main/java/com/koshailimited/springdatajpamapping/entity/Student.java
+++ src/main/java/com/koshailimited/springdatajpamapping/entity/Student.java
@@ -5,20 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "tbl_student", uniqueConstraints = @UniqueConstraint(name = "emailid_unique", columnNames = "email_address"))
 public class Student {
 
     @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private Long studentId;
     private String firstName;
     private String lastName;
+    @Column(nullable = false, name = "email_address")
     private String emailId;
     private String guardianName;
     private String guardianEmail;
